f49d2828e4ffabc9803cd21f93ea7149f4fc6b5b
Stephen Cesar <scesar@student.s19.be>
2026-08-05 23:22:27 +0200

    Bug

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -51,7 +51,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	wp core install --allow-root \
 		--url="${DOMAIN_NAME}" \
 		--title="Inception42" \
-		--admin_user="${WP_ADMIN}" \
+		--admin_user="${WP_ADMIN_USER}" \
 		--admin_password="${WP_ADMIN_PASSWORD}" \
 		--admin_email="${WP_ADMIN_EMAIL}" \
 		--path="/var/www/html/"
